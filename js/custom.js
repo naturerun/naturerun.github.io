7133216a1954658ecae80aa9f479035949c2cf26
naturerun <wsk18061614547@outlook.com>
2019-04-26 04:24:43 +0800

    Site updated: 2019-04-26 04:24:27

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+/* 轮播背景图片 */
+$(function () {
+    $.backstretch([  
+          "/images/background/1.jpg",
+          "/images/background/2.jpg",
+          "/images/background/3.jpg",
+		  "/images/background/4.jpg",
+		  "/images/background/5.jpg"
+    ], { duration: 120000, fade: 1500 });  
+});
