eff581915ef88d696e809ae5f74f9ffee3ad7988
naturerun <wsk18061614547@outlook.com>
2019-05-06 09:35:24 +0800

    Site updated: 2019-05-06 09:34:57

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,24 @@ $(function () {
           "/images/background/2.jpg",
           "/images/background/3.jpg",
 		  "/images/background/4.jpg",
-		  "/images/background/5.jpg"
-    ], { duration: 120000, fade: 1500 });  
+		  "/images/background/5.jpg",
+		  "/images/background/6.jpg",
+		  "/images/background/9.jpg",
+		  "/images/background/10.jpg",
+		  "/images/background/11.jpg",
+		  "/images/background/12.jpg",
+		  "/images/background/13.jpg",
+		  "/images/background/14.jpg",
+		  "/images/background/15.jpg",
+		  "/images/background/16.jpg",
+		  "/images/background/17.jpg",
+		  "/images/background/18.jpg",
+		  "/images/background/19.jpg",
+		  "/images/background/20.jpg",
+		  "/images/background/21.jpg",
+		  "/images/background/22.jpg",
+		  "/images/background/23.jpg",
+		  "/images/background/24.jpg",
+		  "/images/background/25.jpg"
+    ], { duration: 180000, fade: 3000 });  
 });
